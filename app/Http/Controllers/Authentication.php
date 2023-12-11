2a93f9a093a0eae3770e5af8d3eedc1361982b5d
Daljeet singh <daljeetsingh9911@gmail.com>
2023-12-11 21:32:54 +0100

    add controllers and Request for front

---
+++ app/Http/Controllers/Authentication.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegistrationRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class Authentication extends Controller
+{
+    public function registration(RegistrationRequest $request){
+        $data = $request->validated();
+
+        /** @var \App\Models\User */
+        $user = User::create([
+            "name"=>$data["name"],
+            "email"=> $data["email"],
+            "password"=> bcrypt($data["password"]),
+        ]);
+
+        $token = $user->createToken("main")->plainTextToken;
+
+        return response([
+            'user' => $user,
+            'token' => $token
+        ]);
+    } 
+
+    public function login(LoginRequest $request){
+        $credentials = $request->validated();
+        $rememberMe = $credentials['rememberMe'] ?? false;
+
+        unset($credentials['rememberMe']);
+
+        if(!Auth::attempt($credentials, $rememberMe)){
+            return response([
+                'error' =>'incorrect credentials'
+            ],442);
+        }
+
+        /** @var \App\Models\User */
+        $user = Auth::user();
+
+        $token = $user->createToken("main")->plainTextToken;
+
+        return response([
+            'user' => $user,
+            'token' => $token
+        ]);
+        
+    }
+
+    public function logout(Request $request){
+        
+        /** @var User $user*/
+        $user = Auth::user();
+
+        // revoke the token
+
+        $user->currentAccessToken->delete();
+        
+        return response([
+            'success' => true
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Authentication;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/registration', [Authentication::class,'registrations'])->name('Registrations');
+Route::get('/login', [Authentication::class,'login'])->name('Login');
+Route::get('/logout', [Authentication::class,'logout'])->name('Logout');
+
