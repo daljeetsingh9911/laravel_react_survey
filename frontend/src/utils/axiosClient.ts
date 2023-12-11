31b4de7c0f56f44c1ef07a6417898a6d510a0954
Daljeet singh <daljeetsingh9911@gmail.com>
2023-12-11 20:54:13 +0100

    create axios interceptors for api calls

---
+++ frontend/src/utils/axiosClient.ts
@@ -0,0 +1,28 @@
+import axios, { AxiosInstance, AxiosResponse } from 'axios';
+
+const axiosClient: AxiosInstance = axios.create({
+  baseURL: `${import.meta.env.VITE_API_BASE_URL}/api/`,
+});
+
+axiosClient.interceptors.request.use(
+  (config) => {
+    const token = 'Token ';
+    config.headers.Authorization = `Basic ${token}`;
+    return config;
+  },
+);
+
+axiosClient.interceptors.response.use(
+  (response: AxiosResponse) => {
+    return response;
+  },
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      window.location.href = `${window.location.origin}/login`;
+      return Promise.reject(error);
+    }
+    return Promise.reject(error);
+  },
+);
+
+export default axiosClient;
