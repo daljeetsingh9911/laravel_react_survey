3ae8196a8955083198fd254fbab65e10f74eb049
Daljeet singh <daljeetsingh9911@gmail.com>
2023-12-16 11:44:56 +0100

    refactor validation

--- frontend/src/utils/ValidationObject.tsx
+++ frontend/src/utils/ValidationObject.tsx
@@ -1,21 +1,22 @@
 import * as Yup from 'yup';
-import { InputFieldTypes } from './initValues';
+import { showOptionIf } from './initValues';
+
 export const LoginValidation = Yup.object({
-        password: Yup.string()
-                .min(5, 'Must be more than 5 characters')
-                .required('The password is required'),
-        email: Yup.string().email('Invalid email address').required('Email address is required'),
+  password: Yup.string()
+    .min(5, 'Must be more than 5 characters')
+    .required('The password is required'),
+  email: Yup.string().email('Invalid email address').required('Email address is required'),
 })
 
 
 export const RegistrarionValidation = Yup.object({
   password: Yup.string()
-          .min(5, 'Must be more than 5 characters')
-          .required('The password is required'),
+    .min(5, 'Must be more than 5 characters')
+    .required('The password is required'),
   email: Yup.string().email('Invalid email address').required('Email address is required'),
   fullName: Yup.string()
-          .required('Name of the user is required')
-          .min(5, 'Name Must be 5 characters or more'),
+    .required('Name of the user is required')
+    .min(5, 'Name Must be 5 characters or more'),
   confirmed: Yup.string()
     .required('The Confirm password field is required')
     .oneOf([Yup.ref('password')], 'Password must match')
@@ -30,23 +31,25 @@ export const CreateSurveyValidation = Yup.object({
     .required('The Description is required').min(10, 'The Description should have at least 10 characters'),
   expire_date: Yup.string()
     .required('The expire date is required'),
-  
+  questions: Yup.array().test('hasQuestions', 'At least one question is required', function (value): any {
+    // Check if questions array is non-empty
+    if (value && value.length > 0) {
+      // Validate each question
+      return Yup.array().of(
+        Yup.object().shape({
+          question: Yup.string().required('Question is required'),
+          description: Yup.string().required('Description is required'),
+          type: Yup.string(),
+          data: Yup.array().test('options', 'At least two non-empty options are required for checkboxes, radios, and selects', function (dataValue) {
+            const type = this.parent.type;
+            if (showOptionIf.includes(type)) {
+              return Array.isArray(dataValue) && dataValue.length >= 2 && dataValue.every(option => option !== '' && option !== null);
+            }
+            return true; // If not checkbox, radio, or select, no specific requirement
+          }),
+        }),
+      ).validateSync(value, { abortEarly: false });
+    }
+    return true; // If questions array is empty, no specific validation
+  }).min(1, 'At least one question is required'),
 });
-// .when('type', {
-//   is: (type:string) => InputFieldTypes.includes(type),
-//   then: Yup.array().min(2, 'At least one option is required').required('Options are required'),
-//   otherwise: Yup.array().notRequired(),
-// }),
-
-// questions: Yup.array().of(
-//   Yup.object().shape({
-//       question: Yup.string().required('Question is required'),
-//       description: Yup.string().required('Description is required'),
-//       type: Yup.string(),
-//       data: Yup.array().when('type',{
-//             is: ((type: string) => InputFieldTypes.includes(type)),
-//             then: Yup.array().min(2, 'At least one option is required'),
-//             otherwise: Yup.array().notRequired(),
-//           })
-//     })
-// ).min(1, 'At least one question is required'),
