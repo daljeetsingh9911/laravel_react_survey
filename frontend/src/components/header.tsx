f39516df0769e85707991d9f2c6330e42508ee2e
Daljeet singh <daljeetsingh9911@gmail.com>
2023-12-21 16:18:03 +0100

    Logout changes

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -4,13 +4,15 @@ import Navbar from 'react-bootstrap/Navbar';
 import Logo from '../assets/survey.png';
 import { Link, NavLink } from 'react-router-dom';
 import axiosClient from '../utils/axiosClient';
-import { useContext } from 'react';
-import { MyContext, removeLocalStorageData } from '../context/surveyContext';
+import { removeLocalStorageData } from '../context/surveyContext';
+import { useState } from 'react';
+import { ShowSuceessMessage } from '../utils/SweetAlert';
+import { Spinner } from 'react-bootstrap';
 
 
 const ResponsiveAppBar = () => {
-
-  const {} = useContext(MyContext);
+  const [loading,setLoading] = useState<boolean>(false);
+  
   const returnActiveClass = ({ isActive }: any) => {
     return isActive
       ? "nav-link active"
@@ -18,13 +20,15 @@ const ResponsiveAppBar = () => {
   }
 
   const logout = () =>{
+    setLoading(true);
     axiosClient.post('logout').then((resp)=>{
       if(resp.data.success){
          removeLocalStorageData();
+         ShowSuceessMessage('See you soon!')
       }
     }).catch((err) =>{
         removeLocalStorageData();
-    })
+  }).finally(() => {setLoading(false);})
   }
 
 
@@ -32,22 +36,22 @@ const ResponsiveAppBar = () => {
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
         <Navbar.Brand>
-          <Link className='nav-link ' to={'/dashboard/home'}>
+          <Link className='nav-link ' to={'/home'}>
             <img src={Logo} alt='Logo' height={60} />
           </Link>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <NavLink className={returnActiveClass} to={'/dashboard/home'}>Home</NavLink>
-            <NavLink className={returnActiveClass} to={'/dashboard/surveys'}>Surveys</NavLink>
+            <NavLink className={returnActiveClass} to={'/home'}>Home</NavLink>
+            <NavLink className={returnActiveClass} to={'/surveys'}>Surveys</NavLink>
           </Nav>
         </Navbar.Collapse>
         <Navbar.Collapse className="justify-content-end gap-2" >
           Logout 
-          <a onClick={logout} role="button" className='border border-1 rounded-circle bg-default flex justify-content-center align-items-center fs-4 ' style={{ height:'50px',width:'50px'}} >
-             <i className="bi bi-box-arrow-right"></i>
-          </a>
+          <button disabled={loading} onClick={logout} role="button" className='border border-1 rounded-circle bg-default flex justify-content-center align-items-center fs-4 ' style={{ height:'50px',width:'50px'}} >
+            {loading?<Spinner/>:(<i className="bi bi-box-arrow-right"></i>)} 
+          </button>
         </Navbar.Collapse>
       </Container>
     </Navbar>
